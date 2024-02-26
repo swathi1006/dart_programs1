1782b838641821848bbfae4314cff9093cb81da2
swathi1006 <swathisahadevan996@gmail.com>
2024-02-26 10:43:09 +0530

    Initial commit

---
+++ bin/sample.dart
@@ -0,0 +1,12 @@
+void main(){
+  // print('swathi');
+  String firstname = 'Swathi';
+  int age = 26;
+  double cgpa = 8.88;
+  String email = 'swathisahadevan610@gmail.com';
+  print('My name is $firstname');
+  print('Age : $age' );
+  print('cgpa : $cgpa');
+  print('Email : $email');
+
+}
